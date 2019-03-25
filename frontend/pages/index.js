b9d4c3bf88e0d4e37f6a4de08981075180e66004
syntony <anthony.synenko@gmail.com>
2019-03-25 18:11:24 +0200

    lesson 6 - 17:46

---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class Home extends Component{
+
+  render() {
+    return (
+      <div>
+        <p>Hey</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class Sell extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>
+          Sell
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Sell;
