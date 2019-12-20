ee63c0121863512a91bce53b37a21796d3427155
Anton Synenko <anthony.synenko@gmail.com>
2019-12-20 11:12:16 +0200

    lesson 15

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,22 @@
-const mutations = {};
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    // TODO Check if user is logged in
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem({
+      data: { ...args }
+    }, info);
+
+    console.log(item)
+
+    return item
+  }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   // create dog!
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
+};
+
+module.exports = Mutation;
